7f72feb16977a3111e3b6a6238cd932747039721
Anthony Frugier <fru.anthony@gmail.com>
2017-07-21 17:09:45 +0200

    liaison avec la BDD H2

--- src/test/java/fr/pizzeria/dao/PizzaDataBaseTest.java
+++ src/test/java/fr/pizzeria/dao/PizzaDataBaseTest.java
@@ -3,7 +3,6 @@ package fr.pizzeria.dao;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -29,12 +28,6 @@ public class PizzaDataBaseTest {
 
 	@BeforeClass
 	public static void setUp() throws Exception {
-		connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
-		statement = connection.createStatement();
-
-		statement.execute("CREATE TABLE `pizza` (`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,"
-				+ "`code` varchar(10), `nom` varchar(255) NOT NULL, `prix` double NOT NULL,"
-				+ "`categorie` integer(3) NOT NULL);");
 
 		listePizza = new ArrayList<>();
 
@@ -46,21 +39,6 @@ public class PizzaDataBaseTest {
 		listePizza.add(new Pizza("SAV", "La savoyarde", 13.00, CategoriePizza.VIANDE));
 		listePizza.add(new Pizza("ORI", "L'orientale", 13.50, CategoriePizza.VIANDE));
 		listePizza.add(new Pizza("CHA", "Champetre", 14.00, CategoriePizza.VEGETARIENNE));
-
-		for (Pizza pizza : listePizza) {
-			insertPizza = connection
-					.prepareStatement("INSERT INTO pizza(code, nom, prix, categorie) VALUES (?, ?, ?, ?);");
-			insertPizza.setString(1, pizza.getCode());
-			insertPizza.setString(2, pizza.getNom());
-			insertPizza.setDouble(3, pizza.getPrix());
-			insertPizza.setInt(4, pizza.getCategoriePizza().ordinal());
-			insertPizza.executeUpdate();
-		}
-	}
-
-	@Test
-	public void testConnection() throws Exception {
-		assertThat(connection.isValid(0)).isEqualTo(true);
 	}
 
 	@Test
